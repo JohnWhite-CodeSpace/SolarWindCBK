65fcb369d84f5e799060aee59256ef9e69b57b37
Jan Biały <janbialy22@gmail.com>
2023-11-27 10:13:31 +0100

    Updated DataSorting2stage.py

--- DataSorting2stage.py
+++ DataSorting2stage.py
@@ -32,58 +32,15 @@ def FindCorrectDataSets(self,SortedDirectory,Terminal, SubSortedDirectory, Progr
                 pathlib.Path(correctfilepath).mkdir(parents=True, exist_ok=True)
                 with open(filepath, "r") as filedata:
                     lines = filedata.readlines()
+                    filedata.close()
                 output_lines = process_lines(lines)
                 if output_lines:
-                    with open(correctfilepath, 'w') as output_file:
+                    with open(os.path.join(correctfilepath, file), 'w') as output_file:
                         output_file.writelines(output_lines)
+                        output_file.close()
 
-def setProgressBar(datasize):
-    return datasize
 
 def process_lines(lines):
-    # Add your conditions for processing lines here
-    # For example, let's copy lines containing the word 'example'
     return [line for line in lines if re.split('\s+', line)[4] in ["0A", "0E", "05", "40", "2*"]]
-def ScanningProcess(self,lines,Terminal,ProgressBar,correctfilepath,file,filepath):
-    datasize = len(lines)
-    count = 0
-    self.update_progress_signal.emit(count)
-    ProgressBar.setMaximum(datasize)
-
-    for line in lines:
-        if count%100==0:
-            print(count)
-            print(datasize)
-        try:
-            count += 1
-            self.update_progress_signal.emit(count)
-        except Exception as e:
-            print(e)
-            Terminal.append(f"An error occurred while updating progress: {str(e)}")
-            time.sleep(0.1)
-            continue
-        eventtype = re.split('\s+', line)[4]
-        if eventtype in ["0A", "0E", "05", "40", "2*"]:
-            #Terminal.append(f"Event type found: {eventtype} in line of data: {line}")
-            try:
-                with open(os.path.join(correctfilepath, file), "a") as correctfile:
-                    correctfile.write(line + "\n")
-            except Exception as e:
-                print(e)
-                Terminal.append(f"An error occurred while opening file or writing to file: {str(e)}")
-                correctfile.close()
-                time.sleep(0.1)
-                continue
-            # except Exception as e:
-            #     print(e)
-            #     Terminal.append(f"Error occured while opening file: {str(e)}")
-            #     correctfile.close()
-            #     time.sleep(0.1)
-            #     continue
-
-            finally:
-                correctfile.close()
-        #time.sleep(0.0008)
-    Terminal.append(f"\nFinished scanning file: {filepath}")
 
     #Process finished with exit code -1073741819 (0xC0000005)
