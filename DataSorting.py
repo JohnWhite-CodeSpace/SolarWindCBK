5efa751b3564e18e35a5bf7ea1bac61634e87da9
Jan Biały <janbialy22@gmail.com>
2024-01-02 21:29:21 +0100

    Beta version, finalized third stage sorting algorithm

--- DataSorting.py
+++ DataSorting.py
@@ -2,16 +2,17 @@ import sys, os, glob, shutil, pathlib
 import time
 def InitDataFileSorting(self,Source_Directory, Sorted_Directory, Terminal, Label):
     count = 0
-    for root, dirs, files in os.walk(Source_Directory):
-        Label.setText(f"Scanning directory: {root}")
-        if any(file.endswith(".attdba") for file in files if os.path.isfile(os.path.join(root, file))) \
-                and not any(file.endswith(".attd2a") for file in files if os.path.isfile(os.path.join(root, file))):
-            source_directory = os.path.abspath(root)
-            Terminal.append(f"Found '.attdba' file in: {source_directory}")
-            destination_directory = os.path.join(Sorted_Directory, os.path.relpath(source_directory, Source_Directory))
-            Label.setText(f"Copying data from {source_directory} to {destination_directory}...")
-            Terminal.append("Copying correct data files for further processing...")
-            CollectFiles(source_directory, destination_directory, Terminal)
+    # for root, dirs, files in os.walk(Source_Directory):
+    #     Label.setText(f"Scanning directory: {root}")
+    #     if any(file.endswith(".attdba") for file in files if os.path.isfile(os.path.join(root, file))) \
+    #             and not any(file.endswith(".attd2a") for file in files if os.path.isfile(os.path.join(root, file))):
+    #         source_directory = os.path.abspath(root)
+    #         Terminal.append(f"Found '.attdba' file in: {source_directory}")
+    #         destination_directory = os.path.join(Sorted_Directory, os.path.relpath(source_directory, Source_Directory))
+    #         Label.setText(f"Copying data from {source_directory} to {destination_directory}...")
+    #         Terminal.append("Copying correct data files for further processing...")
+    #         CollectFiles(source_directory, destination_directory, Terminal)
+    CheckDoubleObservation(Source_Directory,Terminal,Label,Sorted_Directory)
 
 def ignore_directories(dir, files):
     return [d for d in files if os.path.isdir(os.path.join(dir, d))]
@@ -23,6 +24,37 @@ def CollectFiles(Source_Directory, Sorted_Directory, Terminal):
                 Terminal.append(f"Dataset successfully copied from {Source_Directory} to {Sorted_Directory}")
             except:
                 Terminal.append("Cannot create a file when that file already exists: ")
+
+def CheckDoubleObservation(SourceDirectory, Terminal, Label, SortedDirectory):
+    for root, dirs, files in os.walk(SourceDirectory):
+        CheckHi3 = ""
+        CheckHi2 = ""
+        for file in files:
+            if file.endswith("hihb-2.txt"):
+                CheckHi2 = file
+                Terminal.append(f"Found file: {file}.")
+            elif file.endswith("hihb-3.txt"):
+                CheckHi3 = file
+                Terminal.append(f"Found file: {file}.")
+        if CheckHi3 != "" and CheckHi2 != "":
+            if os.path.getsize(os.path.join(root, CheckHi3))>=1.5*os.path.getsize(os.path.join(root,CheckHi2)):
+                Label.setText(f"Double observation occurred in {os.path.relpath(CheckHi3)} ")
+                openfile = os.path.join(SortedDirectory, "DoubleObservationCheck.txt")
+                with open(openfile, 'a') as IsDoubleFile:
+                    IsDoubleFile.write(f"Channel 3 was observed twice in {os.path.relpath(CheckHi3)}.\n")
+                IsDoubleFile.close()
+            else:
+                Label.setText(f"No double observations in {os.path.relpath(CheckHi3)} ")
+                openfile = os.path.join(SortedDirectory, "DoubleObservationCheck.txt")
+                with open(openfile, 'a') as IsDoubleFile:
+                    IsDoubleFile.write(f"No double observations detected in {os.path.relpath(CheckHi3)}.\n")
+                IsDoubleFile.close()
+                time.sleep(0.0005)
+
+
+
+
+
 
 
 
--- DataSorting3stage.py
+++ DataSorting3stage.py
@@ -5,17 +5,18 @@ import re
 
 
 def ProcessHiTimes(self,Terminal, ProgressBar, ProgressLabel, SourceDirectory, SortedDirectory):
-    with open("HiCullGoodTimes", "r") as GoodHi:
-        HIList = GoodHi.readlines()
-        GoodHi.close()
-    for line in HIList:
-        if not line.startswith("#") and line.strip():
-            conditions = re.split("\s+", line)
-            if len(conditions)>=11:
-                ProgressLabel.setText(f"Processing data conditions: {line}")
-                paths = GetHiDir(SourceDirectory, SortedDirectory, conditions)
-                ProcessDataHi(self,conditions, paths[1], paths[0], Terminal, paths[2], ProgressBar)
+    # with open("HiCullGoodTimes", "r") as GoodHi:
+    #     HIList = GoodHi.readlines()
+    #     GoodHi.close()
+    # for line in HIList:
+    #     if not line.startswith("#") and line.strip():
+    #         conditions = re.split("\s+", line)
+    #         if len(conditions)>=11:
+    #             ProgressLabel.setText(f"Processing data conditions: {line}")
+    #             paths = GetHiDir(SourceDirectory, SortedDirectory, conditions)
+    #             ProcessDataHi(self,conditions, paths[1], paths[0], Terminal, paths[2], ProgressBar)
     SumUpHi(self, Terminal,ProgressBar,ProgressLabel,SortedDirectory)
+    # CheckDoubleObservation(SourceDirectory, SortedDirectory)
 
 def GetHiDir(SourceDirectory, SortedDirectory, Conditions):
     CorrectFP = None
@@ -37,6 +38,8 @@ def ProcessDataHi(self,Conditions, pathG, CorrectPath, Terminal, subdirectory, P
     count = 0
     ProgressBar.setMaximum(len(Intgoodfiles))
     self.update_progress_signal.emit(count, 1)
+    lines = 0
+    nums = None
     for openf in Intgoodfiles:
         count += 1
         if openf==1 and subdirectory is not None:
@@ -47,13 +50,14 @@ def ProcessDataHi(self,Conditions, pathG, CorrectPath, Terminal, subdirectory, P
                     nums = os.path.join(CorrectPath, "GoodTimesCounterHi.txt")
                     out = os.path.join(CorrectPath, os.path.basename(file.name))
                     lines = file.readlines()
-                    ProcessLinesHi(self, lines, out, nums, Conditions, ProgressBar)
                     file.close()
+                    ProcessLinesHi(lines, out, nums, Conditions)
             except FileNotFoundError:
                 pass
         self.update_progress_signal.emit(count, 1)
 
-def ProcessLinesHi(self,lines, OutputFile, CounterFile, Conditions, ProgressBar):
+
+def ProcessLinesHi(lines, OutputFile, CounterFile, Conditions):
     goodlines = 0
     GoodTimes = []
     if not os.path.isfile(CounterFile):
@@ -66,10 +70,10 @@ def ProcessLinesHi(self,lines, OutputFile, CounterFile, Conditions, ProgressBar)
             if float(Conditions[1]) <= float(temparray[0]) <= float(Conditions[2]):
                 GoodTimes.append(line)
                 goodlines += 1
-        with open(OutputFile, "a") as out:
+        with open(OutputFile, "a") as Out:
             for i in GoodTimes:
-                out.write(i)
-            out.close()
+                Out.write(i)
+            Out.close()
         with open(CounterFile, "a") as Fcount:
             deltaT = float(Conditions[2]) - float(Conditions[1])
             freq = goodlines/deltaT
@@ -77,16 +81,16 @@ def ProcessLinesHi(self,lines, OutputFile, CounterFile, Conditions, ProgressBar)
             Fcount.close()
 
 def ProcessLoTimes(self,Terminal, ProgressBar, ProgressLabel, SourceDirectory, SortedDirectory):
-    with open("LoGoodTimes.txt", "r") as GoodLo:
-        LoList = GoodLo.readlines()
-        GoodLo.close()
-    for line in LoList:
-        if not line.startswith("#") and line.strip():
-            conditions = re.split("\s+", line)
-            if len(conditions)>=11:
-                ProgressLabel.setText(f"Processing data conditions: {line}")
-                paths = GetLoDir(SourceDirectory, SortedDirectory, conditions)
-                ProcessDataLo(self,conditions, paths[1], paths[0], Terminal, paths[2], ProgressBar)
+    # with open("LoGoodTimes.txt", "r") as GoodLo:
+    #     LoList = GoodLo.readlines()
+    #     GoodLo.close()
+    # for line in LoList:
+    #     if not line.startswith("#") and line.strip():
+    #         conditions = re.split("\s+", line)
+    #         if len(conditions)>=11:
+    #             ProgressLabel.setText(f"Processing data conditions: {line}")
+    #             paths = GetLoDir(SourceDirectory, SortedDirectory, conditions)
+    #             ProcessDataLo(self,conditions, paths[1], paths[0], Terminal, paths[2], ProgressBar)
     SumUpLo(self, Terminal, ProgressBar, ProgressLabel, SortedDirectory)
 def GetLoDir(SourceDirectory, SortedDirectory, Conditions):
     CorrectFP = None
@@ -108,6 +112,9 @@ def ProcessDataLo(self,Conditions, pathG, CorrectPath, Terminal, subdirectory, P
     count = 0
     ProgressBar.setMaximum(len(Intgoodfiles))
     self.update_progress_signal.emit(count, 2)
+    lines = 0
+    out = []
+    nums = None
     for openf in Intgoodfiles:
         count += 1
         if openf==1 and subdirectory is not None:
@@ -118,13 +125,14 @@ def ProcessDataLo(self,Conditions, pathG, CorrectPath, Terminal, subdirectory, P
                     nums = os.path.join(CorrectPath, "GoodTimesCounterLo.txt")
                     out = os.path.join(CorrectPath, os.path.basename(file.name))
                     lines = file.readlines()
-                    ProcessLinesLo(self, lines, out, nums, Conditions)
                     file.close()
+                    ProcessLinesLo(lines, out, nums, Conditions)
             except FileNotFoundError:
                 pass
         self.update_progress_signal.emit(count, 2)
 
-def ProcessLinesLo(self,lines, OutputFile, CounterFile, Conditions):
+
+def ProcessLinesLo(lines, OutputFile, CounterFile, Conditions):
     goodlines = 0
     GoodTimes = []
     if not os.path.isfile(CounterFile):
@@ -137,10 +145,10 @@ def ProcessLinesLo(self,lines, OutputFile, CounterFile, Conditions):
             if float(Conditions[1]) <= float(temparray[0]) <= float(Conditions[2]):
                 GoodTimes.append(line)
                 goodlines += 1
-        with open(OutputFile, "a") as out:
+        with open(OutputFile, "a") as Out:
             for i in GoodTimes:
-                out.write(i)
-            out.close()
+                Out.write(i)
+            Out.close()
         with open(CounterFile, "a") as Fcount:
             deltaT = float(Conditions[2]) - float(Conditions[1])
             freq = goodlines/deltaT
@@ -159,35 +167,35 @@ def SumUpHi(self, Terminal, ProgressBar, ProgressLabel, SourceDirectory):
                     next(counter)
                     lines = counter.read().split('\n')
                     counter.close()
-                path = os.path.join(root,"SumHI.txt")
+                path = os.path.join(root,"SumHICorrect.txt")
                 ProgressLabel.setText(f"Summing up file: {file}")
                 CalcSumHi(self,lines,path, ProgressBar)
 
 def CalcSumHi(self, lines, path, ProgressBar):
     hide_counts = {}
-    # count = 0
-    # ProgressBar.setMaximum(len(lines))
-    # self.update_progress_signal.emit(count, 1)
     for line in lines:
-        # count += 1
-        # self.update_progress_signal.emit(count, 1)
         fields = re.split('\t', line)
         location = fields[-1]
         print(f"{fields}+\n")
         hide_file = re.search(r'hide-(\d+).txt', location)
         if hide_file:
+            multiplyer = 1/6
+            if "hide-3.txt" in location:
+                if GetMultiplyerConstant(os.path.abspath(location)):
+                    multiplyer = 1/3
             hide_number = hide_file.group(1)
             countT = int(fields[2])
             deltaT = float(fields[1]) - float(fields[0])
-            hide_counts[hide_number] = hide_counts.get(hide_number, (0, 0))
+            hide_counts[hide_number] = hide_counts.get(hide_number, (0, 0, 0))
             hide_counts[hide_number] = (
                 hide_counts[hide_number][0] + countT,
-                hide_counts[hide_number][1] + deltaT
+                hide_counts[hide_number][1] + deltaT,
+                multiplyer
             )
     with open(path, "a") as sum:
-        sum.write("File:\tCount:\tInverse Delta T:\t Frequency [counts/deltaT]:\n")
-        for hide_number, (count, invdelta) in hide_counts.items():
-            sum.write(f"hide-{hide_number}\t{count}\t{invdelta}\t{count/invdelta}\n")
+        sum.write("File:\tCount:\tInverse Delta T:\t Frequency [counts/deltaT]:\t Multiplyer:\n")
+        for hide_number, (count, invdelta, mul) in hide_counts.items():
+            sum.write(f"hide-{hide_number}\t{count}\t{invdelta}\t{mul*(count/invdelta)}\t{mul}\n")
 
 def SumUpLo(self, Terminal, ProgressBar, ProgressLabel, SourceDirectory):
     Terminal.append("Summing up Lo GoodTimes...")
@@ -199,26 +207,57 @@ def SumUpLo(self, Terminal, ProgressBar, ProgressLabel, SourceDirectory):
                     next(counter)
                     lines = counter.read().split('\n')
                     counter.close()
-                path = os.path.join(root,"SumLo.txt")
+                path = os.path.join(root,"SumLoCorrect.txt")
                 ProgressLabel.setText(f"Summing up file: {file}")
                 CalcSumLo(self,lines,path, ProgressBar)
 def CalcSumLo(self, lines, path, ProgressBar):
-    hide_counts = {}
+    lode_counts = {}
     for line in lines:
         fields = re.split('\t', line)
         location = fields[-1]
-        print(f"{fields}+\n")
-        hide_file = re.search(r'lode-(\d+).txt', location)
-        if hide_file:
-            hide_number = hide_file.group(1)
+        #print(f"{fields}+\n")
+        lode_file = re.search(r'lode-(\d+).txt', location)
+        if lode_file:
+            lode_number = lode_file.group(1)
             countT = int(fields[2])
             deltaT = float(fields[1]) - float(fields[0])
-            hide_counts[hide_number] = hide_counts.get(hide_number, (0, 0))
-            hide_counts[hide_number] = (
-                hide_counts[hide_number][0] + countT,
-                hide_counts[hide_number][1] + deltaT
+            lode_counts[lode_number] = lode_counts.get(lode_number, (0, 0))
+            lode_counts[lode_number] = (
+                lode_counts[lode_number][0] + countT,
+                lode_counts[lode_number][1] + deltaT
             )
     with open(path, "a") as sum:
         sum.write("File:\tCount:\tInverse Delta T:\t Frequency [counts/deltaT]:\n")
-        for hide_number, (count, invdelta) in hide_counts.items():
-            sum.write(f"hide-{hide_number}\t{count}\t{invdelta}\t{count / invdelta}\n")
+        for lode_number, (count, invdelta) in lode_counts.items():
+            sum.write(f"lode-{lode_number}\t{count}\t{invdelta}\t{1/8*(count / invdelta)}\n")
+
+
+
+
+def GetMultiplyerConstant(HideFile):
+    IsObservedTwice = False
+    with open("DoubleObservationCheck.txt", 'r') as CheckDoubleFile:
+        lines = CheckDoubleFile.readlines()
+    CheckDoubleFile.close()
+    for line in lines:
+        if "Channel 3 was observed twice in" in line:
+            dirinfo = line.split("Channel 3 was observed twice in ")
+            dirtype = dirinfo[1].split('.')[0]
+            print(dirtype + "   " + HideFile)
+            if dirtype in HideFile:
+                IsObservedTwice = True
+                print("match!!!")
+                break
+    return IsObservedTwice
+
+
+
+
+
+
+
+
+
+
+
+
